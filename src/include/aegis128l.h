5f51212116200b8b3f88d56c86f307649e51c9e3
Frank Denis <github@pureftpd.org>
2023-07-29 19:13:12 +0200

    Fix pasto in comments
    Fixes #1

--- src/include/aegis128l.h
+++ src/include/aegis128l.h
@@ -55,8 +55,8 @@ size_t aegis128l_tailbytes_max(void);
  * mlen: length of the plaintext
  * ad: additional data input buffer
  * adlen: length of the additional data
- * npub: nonce input buffer (32 bytes)
- * k: key input buffer (32 bytes)
+ * npub: nonce input buffer (16 bytes)
+ * k: key input buffer (16 bytes)
  */
 int aegis128l_encrypt_detached(uint8_t *c, uint8_t *mac, size_t maclen, const uint8_t *m,
                                size_t mlen, const uint8_t *ad, size_t adlen, const uint8_t *npub,
@@ -72,8 +72,8 @@ int aegis128l_encrypt_detached(uint8_t *c, uint8_t *mac, size_t maclen, const ui
  * maclen: length of the authentication tag (16 or 32)
  * ad: additional data input buffer
  * adlen: length of the additional data
- * npub: nonce input buffer (32 bytes)
- * k: key input buffer (32 bytes)
+ * npub: nonce input buffer (16 bytes)
+ * k: key input buffer (16 bytes)
  *
  * Returns 0 if the ciphertext is authentic, -1 otherwise.
  */
@@ -90,8 +90,8 @@ int aegis128l_decrypt_detached(uint8_t *m, const uint8_t *c, size_t clen, const 
  * mlen: length of the plaintext
  * ad: additional data input buffer
  * adlen: length of the additional data
- * npub: nonce input buffer (32 bytes)
- * k: key input buffer (32 bytes)
+ * npub: nonce input buffer (16 bytes)
+ * k: key input buffer (16 bytes)
  */
 int aegis128l_encrypt(uint8_t *c, size_t maclen, const uint8_t *m, size_t mlen, const uint8_t *ad,
                       size_t adlen, const uint8_t *npub, const uint8_t *k);
@@ -105,8 +105,8 @@ int aegis128l_encrypt(uint8_t *c, size_t maclen, const uint8_t *m, size_t mlen, 
  * maclen: length of the authentication tag (16 or 32)
  * ad: additional data input buffer
  * adlen: length of the additional data
- * npub: nonce input buffer (32 bytes)
- * k: key input buffer (32 bytes)
+ * npub: nonce input buffer (16 bytes)
+ * k: key input buffer (16 bytes)
  *
  * Returns 0 if the ciphertext is authentic, -1 otherwise.
  */
@@ -120,8 +120,8 @@ int aegis128l_decrypt(uint8_t *m, const uint8_t *c, size_t clen, size_t maclen, 
  * st_: state to initialize
  * ad: additional data input buffer
  * adlen: length of the additional data
- * npub: nonce input buffer (32 bytes)
- * k: key input buffer (32 bytes)
+ * npub: nonce input buffer (16 bytes)
+ * k: key input buffer (16 bytes)
  */
 void aegis128l_state_init(aegis128l_state *st_, const uint8_t *ad, size_t adlen,
                           const uint8_t *npub, const uint8_t *k);
