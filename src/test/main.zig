654e694f1d05bafd688fb6bfdaa3e9b054b7cea2
Frank Denis <github@pureftpd.org>
2023-08-03 19:18:55 +0200

    Add aegis-128l tests

--- src/test/main.zig
+++ src/test/main.zig
@@ -9,7 +9,7 @@ test "aegis-128l - encrypt_detached oneshot" {
     const mac_len: usize = 16;
     const max_msg_len: usize = 1000;
     const max_ad_len: usize = 1000;
-    const iterations = 10000;
+    const iterations = 50000;
 
     var msg_buf: [max_msg_len]u8 = undefined;
     var msg2_buf: [msg_buf.len]u8 = undefined;
@@ -38,6 +38,46 @@ test "aegis-128l - encrypt_detached oneshot" {
 
         var msg2 = msg2_buf[0..msg_len];
         ret = aegis.aegis128l_decrypt_detached(msg2.ptr, c.ptr, c.len, &mac, mac_len, ad.ptr, ad.len, &nonce, &key);
+        try testing.expectEqual(ret, 0);
+        try testing.expectEqualSlices(u8, msg, msg2);
+    }
+}
+
+test "aegis-256 - encrypt_detached oneshot" {
+    try testing.expectEqual(aegis.aegis_init(), 0);
+
+    const mac_len: usize = 16;
+    const max_msg_len: usize = 1000;
+    const max_ad_len: usize = 1000;
+    const iterations = 50000;
+
+    var msg_buf: [max_msg_len]u8 = undefined;
+    var msg2_buf: [msg_buf.len]u8 = undefined;
+    var ad_buf: [max_ad_len]u8 = undefined;
+    var c_buf: [msg_buf.len]u8 = undefined;
+    var mac: [mac_len]u8 = undefined;
+
+    random.bytes(&msg_buf);
+    random.bytes(&ad_buf);
+
+    for (0..iterations) |_| {
+        const msg_len = random.intRangeAtMost(usize, 0, msg_buf.len);
+        var msg = msg_buf[0..msg_len];
+        var c = c_buf[0..msg_len];
+
+        const ad_len = random.intRangeAtMost(usize, 0, ad_buf.len);
+        const ad = &ad_buf[0..ad_len];
+
+        var nonce: [aegis.aegis256_NPUBBYTES]u8 = undefined;
+        random.bytes(&nonce);
+        var key: [aegis.aegis256_KEYBYTES]u8 = undefined;
+        random.bytes(&key);
+
+        var ret = aegis.aegis256_encrypt_detached(c.ptr, &mac, mac_len, msg.ptr, msg.len, ad.ptr, ad.len, &nonce, &key);
+        try testing.expectEqual(ret, 0);
+
+        var msg2 = msg2_buf[0..msg_len];
+        ret = aegis.aegis256_decrypt_detached(msg2.ptr, c.ptr, c.len, &mac, mac_len, ad.ptr, ad.len, &nonce, &key);
         try testing.expectEqual(ret, 0);
         try testing.expectEqualSlices(u8, msg, msg2);
     }
