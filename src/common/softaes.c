72ab431e558859209de0951263ca96f24894d426
Frank Denis <github@pureftpd.org>
2023-08-05 08:38:44 +0200

    Add a FAVOR_PERFORMANCE knob

--- src/common/softaes.c
+++ src/common/softaes.c
@@ -6,7 +6,7 @@
 #include "common.h"
 #include "softaes.h"
 
-uint32_t _aes_lut[256] __attribute__ ((visibility ("hidden"))) = {
+uint32_t _aes_lut[256] __attribute__((visibility("hidden"))) = {
     0xa56363c6, 0x847c7cf8, 0x997777ee, 0x8d7b7bf6, 0x0df2f2ff, 0xbd6b6bd6, 0xb16f6fde, 0x54c5c591,
     0x50303060, 0x03010102, 0xa96767ce, 0x7d2b2b56, 0x19fefee7, 0x62d7d7b5, 0xe6abab4d, 0x9a7676ec,
     0x45caca8f, 0x9d82821f, 0x40c9c989, 0x877d7dfa, 0x15fafaef, 0xeb5959b2, 0xc947478e, 0x0bf0f0fb,
@@ -41,10 +41,14 @@ uint32_t _aes_lut[256] __attribute__ ((visibility ("hidden"))) = {
     0xc3414182, 0xb0999929, 0x772d2d5a, 0x110f0f1e, 0xcbb0b07b, 0xfc5454a8, 0xd6bbbb6d, 0x3a16162c
 };
 
-static const uint32_t * const LUT = _aes_lut;
+static const uint32_t* const LUT = _aes_lut;
 
 #ifndef SOFTAES_STRIDE
+#ifdef FAVOR_PERFORMANCE
+#define SOFTAES_STRIDE 256
+#else
 #define SOFTAES_STRIDE 16
+#endif
 #endif
 
 static SoftAesBlock
