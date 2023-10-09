d8089184a7c598494cc2daa0c2b63499f149f7a1
Frank Denis <github@pureftpd.org>
2023-10-09 16:32:29 +0200

    Benchmark AEGIS as a MAC

--- src/test/benchmark.zig
+++ src/test/benchmark.zig
@@ -200,6 +200,105 @@ fn bench_aegis128x4() !void {
     try stdout.print("AEGIS-128X4\t{d:10.2} Mb/s\n", .{throughput});
 }
 
+fn bench_aegis128l_mac() !void {
+    var key: [aegis.aegis128l_KEYBYTES]u8 = undefined;
+    var nonce: [aegis.aegis128l_NPUBBYTES]u8 = undefined;
+    var buf: [msg_len]u8 = undefined;
+
+    random.bytes(&key);
+    random.bytes(&nonce);
+    random.bytes(&buf);
+
+    var timer = try Timer.start();
+    const start = timer.lap();
+    for (0..iterations) |_| {
+        _ = aegis.aegis128l_encrypt_detached(
+            null,
+            &buf,
+            aegis.aegis128l_ABYTES_MAX,
+            null,
+            0,
+            &buf,
+            msg_len,
+            &nonce,
+            &key,
+        );
+    }
+    const end = timer.read();
+    mem.doNotOptimizeAway(buf[0]);
+    const bits: f128 = @floatFromInt(@as(u128, msg_len) * iterations * 8);
+    const elapsed_s = @as(f128, @floatFromInt(end - start)) / time.ns_per_s;
+    const throughput = @as(f64, @floatCast(bits / (elapsed_s * 1000 * 1000)));
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("AEGIS-128L MAC\t{d:10.2} Mb/s\n", .{throughput});
+}
+
+fn bench_aegis128x2_mac() !void {
+    var key: [aegis.aegis128x2_KEYBYTES]u8 = undefined;
+    var nonce: [aegis.aegis128x2_NPUBBYTES]u8 = undefined;
+    var buf: [msg_len]u8 = undefined;
+
+    random.bytes(&key);
+    random.bytes(&nonce);
+    random.bytes(&buf);
+
+    var timer = try Timer.start();
+    const start = timer.lap();
+    for (0..iterations) |_| {
+        _ = aegis.aegis128x2_encrypt_detached(
+            null,
+            &buf,
+            aegis.aegis128x2_ABYTES_MAX,
+            null,
+            0,
+            &buf,
+            msg_len,
+            &nonce,
+            &key,
+        );
+    }
+    const end = timer.read();
+    mem.doNotOptimizeAway(buf[0]);
+    const bits: f128 = @floatFromInt(@as(u128, msg_len) * iterations * 8);
+    const elapsed_s = @as(f128, @floatFromInt(end - start)) / time.ns_per_s;
+    const throughput = @as(f64, @floatCast(bits / (elapsed_s * 1000 * 1000)));
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("AEGIS-128X2 MAC\t{d:10.2} Mb/s\n", .{throughput});
+}
+
+fn bench_aegis128x4_mac() !void {
+    var key: [aegis.aegis128x4_KEYBYTES]u8 = undefined;
+    var nonce: [aegis.aegis128x4_NPUBBYTES]u8 = undefined;
+    var buf: [msg_len]u8 = undefined;
+
+    random.bytes(&key);
+    random.bytes(&nonce);
+    random.bytes(&buf);
+
+    var timer = try Timer.start();
+    const start = timer.lap();
+    for (0..iterations) |_| {
+        _ = aegis.aegis128x4_encrypt_detached(
+            null,
+            &buf,
+            aegis.aegis128x4_ABYTES_MAX,
+            null,
+            0,
+            &buf,
+            msg_len,
+            &nonce,
+            &key,
+        );
+    }
+    const end = timer.read();
+    mem.doNotOptimizeAway(buf[0]);
+    const bits: f128 = @floatFromInt(@as(u128, msg_len) * iterations * 8);
+    const elapsed_s = @as(f128, @floatFromInt(end - start)) / time.ns_per_s;
+    const throughput = @as(f64, @floatCast(bits / (elapsed_s * 1000 * 1000)));
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("AEGIS-128X4 MAC\t{d:10.2} Mb/s\n", .{throughput});
+}
+
 pub fn main() !void {
     try bench_aegis256();
     try bench_aegis256x2();
@@ -207,4 +306,8 @@ pub fn main() !void {
     try bench_aegis128l();
     try bench_aegis128x2();
     try bench_aegis128x4();
+
+    try bench_aegis128l_mac();
+    try bench_aegis128x2_mac();
+    try bench_aegis128x4_mac();
 }
