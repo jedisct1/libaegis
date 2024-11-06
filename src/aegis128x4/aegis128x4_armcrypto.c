a3a38ec36d94586848bb43dcf2e91be39e5827db
Frank Denis <github@pureftpd.org>
2024-11-06 13:37:15 +0100

    Revert 2ee63911bf710e4ce8863f359b3c29882600e662
    Until tested with concurrency and consistently implemented everywhere

--- src/aegis128x4/aegis128x4_armcrypto.c
+++ src/aegis128x4/aegis128x4_armcrypto.c
@@ -26,9 +26,7 @@
 #        pragma GCC target("+simd+crypto")
 #    endif
 
-#    define AES_BLOCK_LENGTH  64
-#    define AES_BLOCK1_LENGTH 16
-#    define CHUNK_SIZE        512
+#    define AES_BLOCK_LENGTH 64
 
 typedef struct {
     uint8x16_t b0;
@@ -36,83 +34,6 @@ typedef struct {
     uint8x16_t b2;
     uint8x16_t b3;
 } aes_block_t;
-
-/* -- */
-
-typedef uint8x16_t aes_block1_t;
-
-#    define AES_BLOCK1_XOR(A, B)       veorq_u8((A), (B))
-#    define AES_BLOCK1_AND(A, B)       vandq_u8((A), (B))
-#    define AES_BLOCK1_LOAD(A)         vld1q_u8(A)
-#    define AES_BLOCK1_LOAD_64x2(A, B) vreinterpretq_u8_u64(vsetq_lane_u64((A), vmovq_n_u64(B), 1))
-#    define AES_BLOCK1_STORE(A, B)     vst1q_u8((A), (B))
-#    define AES_ENC1(A, B)             veorq_u8(vaesmcq_u8(vaeseq_u8((A), vmovq_n_u8(0))), (B))
-
-static inline void
-aegis128x4_update_b0(aes_block_t *const state, const aes_block1_t d1, const aes_block1_t d2)
-{
-    aes_block1_t tmp;
-
-    tmp         = state[7].b0;
-    state[7].b0 = AES_ENC1(state[6].b0, state[7].b0);
-    state[6].b0 = AES_ENC1(state[5].b0, state[6].b0);
-    state[5].b0 = AES_ENC1(state[4].b0, state[5].b0);
-    state[4].b0 = AES_BLOCK1_XOR(AES_ENC1(state[3].b0, state[4].b0), d2);
-    state[3].b0 = AES_ENC1(state[2].b0, state[3].b0);
-    state[2].b0 = AES_ENC1(state[1].b0, state[2].b0);
-    state[1].b0 = AES_ENC1(state[0].b0, state[1].b0);
-    state[0].b0 = AES_BLOCK1_XOR(AES_ENC1(tmp, state[0].b0), d1);
-}
-
-static inline void
-aegis128x4_update_b1(aes_block_t *const state, const aes_block1_t d1, const aes_block1_t d2)
-{
-    aes_block1_t tmp;
-
-    tmp         = state[7].b1;
-    state[7].b1 = AES_ENC1(state[6].b1, state[7].b1);
-    state[6].b1 = AES_ENC1(state[5].b1, state[6].b1);
-    state[5].b1 = AES_ENC1(state[4].b1, state[5].b1);
-    state[4].b1 = AES_BLOCK1_XOR(AES_ENC1(state[3].b1, state[4].b1), d2);
-    state[3].b1 = AES_ENC1(state[2].b1, state[3].b1);
-    state[2].b1 = AES_ENC1(state[1].b1, state[2].b1);
-    state[1].b1 = AES_ENC1(state[0].b1, state[1].b1);
-    state[0].b1 = AES_BLOCK1_XOR(AES_ENC1(tmp, state[0].b1), d1);
-}
-
-static inline void
-aegis128x4_update_b2(aes_block_t *const state, const aes_block1_t d1, const aes_block1_t d2)
-{
-    aes_block1_t tmp;
-
-    tmp         = state[7].b2;
-    state[7].b2 = AES_ENC1(state[6].b2, state[7].b2);
-    state[6].b2 = AES_ENC1(state[5].b2, state[6].b2);
-    state[5].b2 = AES_ENC1(state[4].b2, state[5].b2);
-    state[4].b2 = AES_BLOCK1_XOR(AES_ENC1(state[3].b2, state[4].b2), d2);
-    state[3].b2 = AES_ENC1(state[2].b2, state[3].b2);
-    state[2].b2 = AES_ENC1(state[1].b2, state[2].b2);
-    state[1].b2 = AES_ENC1(state[0].b2, state[1].b2);
-    state[0].b2 = AES_BLOCK1_XOR(AES_ENC1(tmp, state[0].b2), d1);
-}
-
-static inline void
-aegis128x4_update_b3(aes_block_t *const state, const aes_block1_t d1, const aes_block1_t d2)
-{
-    aes_block1_t tmp;
-
-    tmp         = state[7].b3;
-    state[7].b3 = AES_ENC1(state[6].b3, state[7].b3);
-    state[6].b3 = AES_ENC1(state[5].b3, state[6].b3);
-    state[5].b3 = AES_ENC1(state[4].b3, state[5].b3);
-    state[4].b3 = AES_BLOCK1_XOR(AES_ENC1(state[3].b3, state[4].b3), d2);
-    state[3].b3 = AES_ENC1(state[2].b3, state[3].b3);
-    state[2].b3 = AES_ENC1(state[1].b3, state[2].b3);
-    state[1].b3 = AES_ENC1(state[0].b3, state[1].b3);
-    state[0].b3 = AES_BLOCK1_XOR(AES_ENC1(tmp, state[0].b3), d1);
-}
-
-/* -- */
 
 static inline aes_block_t
 AES_BLOCK_XOR(const aes_block_t a, const aes_block_t b)
